f947a105283c8500c7377963c6d80736c92d1d93
Samus <26967164+SamusElderg@users.noreply.github.com>
2020-10-02 15:27:38 +0800

    minor changes

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { Alert } from 'reactstrap';
 
 export const Context = React.createContext({})
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -158,7 +158,6 @@ const authProtectedRoutes = [
 ];
 
 const publicRoutes = [
-    ,
 
     {path: "/pages-maintenance", component: PagesMaintenance},
     {path: "/pages-comingsoon", component: PagesComingsoon},
