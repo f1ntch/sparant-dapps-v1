83f2e0ebadef4581a28143bbad4a02b455177408
Jael Romero <f1ntch@outlook.com>
2020-10-10 15:59:31 +0200

    Added widgets to pools page.

--- src/pages/Pools.js
+++ src/pages/Pools.js
@@ -24,6 +24,9 @@ import {withNamespaces} from 'react-i18next';
 import CardTitle from "reactstrap/es/CardTitle";
 import CardSubtitle from "reactstrap/es/CardSubtitle";
 import Button from "antd/es/button";
+import Widget from "./Widget";
+import ReactApexChart from "react-apexcharts";
+
 
 const Pools = (props) => {
 
@@ -74,70 +77,271 @@ export default withRouter(withNamespaces()(Pools));
 
 export const PoolsPaneSide = (props) => {
 
-    const context = useContext(Context);
+        const context = useContext(Context);
 
-    return (
 
-        <React.Fragment>
-            <Row>
-                <Col sm={12} md={12}>
-                    <CardWelcome/>
-                </Col>
-            </Row>
-            <Row>
-                <Col sm="3">
 
-                    <Card className="mini-stats-wid">
-                        <CardBody>
-                            <Media>
-                                <Media body>
-                                    <p className="text-muted font-weight-medium">TOTAL POOLED</p>
-                                    <h4 className="mb-0">{formatUSDStatBoxes(convertFromWei(props.globalData.totalPooled * 2), context.spartanPrice)}</h4>
-                                </Media>
-                            </Media>
-                        </CardBody>
-                    </Card>
-                </Col>
-                <Col sm="3">
-                    <Card className="mini-stats-wid">
-                        <CardBody>
-                            <Media>
-                                <Media body>
-                                    <p className="text-muted font-weight-medium">TOTAL VOLUME</p>
-                                    <h4 className="mb-0">{formatUSDStatBoxes(convertFromWei(props.globalData?.totalVolume), context.spartanPrice)}</h4>
-                                </Media>
-                            </Media>
-                        </CardBody>
-                    </Card>
-                </Col>
-                <Col sm="3">
-                    <Card className="mini-stats-wid">
-                        <CardBody>
-                            <Media>
-                                <Media body>
-                                    <p className="text-muted font-weight-medium">TXN COUNT</p>
-                                    <h4 className="mb-0">{+props.globalData?.addLiquidityTx + +props.globalData?.removeLiquidityTx + +props.globalData?.swapTx}</h4>
-                                </Media>
-                            </Media>
-                        </CardBody>
-                    </Card>
-                </Col>
-                <Col sm="3">
-                    <Card className="mini-stats-wid">
-                        <CardBody>
-                            <Media>
-                                <Media body>
-                                    <p className="text-muted font-weight-medium">TOTAL EARNINGS</p>
-                                    <h4 className="mb-0">{formatUSDStatBoxes(convertFromWei(props.globalData?.totalFees), context.spartanPrice)}</h4>
-                                </Media>
-                            </Media>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-        </React.Fragment>
-    )
-};
+        const series1 = [{
+            name: "Pooled",
+            data: [0, formatUSDStatBoxes(convertFromWei(props.globalData.totalPooled * 2), context.spartanPrice).slice(1)]
+        }];
+        const options1 = {
+            chart: {sparkline: {enabled: !0}},
+            stroke: {curve: "smooth", width: 2},
+            colors: ["#f1b44c"],
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shadeIntensity: 1,
+                    inverseColors: !1,
+                    opacityFrom: .45,
+                    opacityTo: .05,
+                    stops: [25, 100, 100, 100]
+                }
+            },
+            tooltip: {fixed: {enabled: !1}, x: {show: !1}, marker: {show: !1}}
+        };
+
+
+        const series2 = [{
+            name: "Volume",
+            data: [0, convertFromWei(props.globalData.totalPooled)]
+        }];
+        const options2 = {
+            chart: {sparkline: {enabled: !0}},
+            stroke: {curve: "smooth", width: 2},
+            colors: ["#3452e1"],
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shadeIntensity: 1,
+                    inverseColors: !1,
+                    opacityFrom: .45,
+                    opacityTo: .05,
+                    stops: [25, 100, 100, 100]
+                }
+            },
+            tooltip: {fixed: {enabled: !1}, x: {show: !1}, marker: {show: !1}}
+        };
+
+        const series3 = [{
+            name: "TXN",
+            data: [0, convertFromWei(props.globalData.totalPooled)]
+        }];
+        const options3 = {
+            chart: {sparkline: {enabled: !0}},
+            stroke: {curve: "smooth", width: 2},
+            colors: ["#50a5f1"],
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shadeIntensity: 1,
+                    inverseColors: !1,
+                    opacityFrom: .45,
+                    opacityTo: .05,
+                    stops: [25, 100, 100, 100]
+                }
+            },
+            tooltip: {fixed: {enabled: !1}, x: {show: !1}, marker: {show: !1}}
+        };
+
+
+        const series4 = [{
+            name: "Earnings",
+            data: [0, convertFromWei(props.globalData.totalPooled)]
+        }];
+        const options4 = {
+            chart: {sparkline: {enabled: !0}},
+            stroke: {curve: "smooth", width: 2},
+            colors: ["#46fc96"],
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shadeIntensity: 1,
+                    inverseColors: !1,
+                    opacityFrom: .45,
+                    opacityTo: .05,
+                    stops: [25, 100, 100, 100]
+                }
+            },
+            tooltip: {fixed: {enabled: !1}, x: {show: !1}, marker: {show: !1}}
+        };
+
+
+        return (
+
+
+            <React.Fragment>
+
+                <Row>
+                    <Col sm={12} md={12}>
+                        <CardWelcome/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm="3">
+                        <Card>
+                            <CardBody>
+                                <h5 className="text-muted mb-4"><i
+                                    className={"bx bx-shape-triangle h1 text-warning align-middle mb-0 mr-3"}></i>Total
+                                    Pooled</h5>
+                                <Row>
+                                    <Col xs="6">
+                                        <div>
+                                            <h5>{formatUSDStatBoxes(convertFromWei(props.globalData.totalPooled * 2), context.spartanPrice)}</h5>
+                                            <p className="text-muted text-truncate mb-0">0,0%<i
+                                                className="mdi mdi-arrow-up ml-1 text-success"></i></p>
+                                        </div>
+                                    </Col>
+                                    <Col xs="6">
+                                        <div>
+                                            <div className="apex-charts">
+                                                <ReactApexChart options={options1} series={series1} type="area"
+                                                                height={40}/>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col sm="3">
+                        <Card>
+                            <CardBody>
+                                <h5 className="text-muted mb-4"><i
+                                    className={"bx bx-coin-stack h1 text-primary align-middle mb-0 mr-3"}></i>Total Volume
+                                </h5>
+                                <Row>
+                                    <Col xs="6">
+                                        <div>
+                                            <h5>{formatUSDStatBoxes(convertFromWei(props.globalData?.totalVolume), context.spartanPrice)}</h5>
+                                            <p className="text-muted text-truncate mb-0">0,0<i
+                                                className="mdi mdi-arrow-up ml-1 text-success"></i></p>
+                                        </div>
+                                    </Col>
+                                    <Col xs="6">
+                                        <div>
+                                            <div className="apex-charts">
+                                                <ReactApexChart options={options2} series={series2} type="area"
+                                                                height={40}/>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col sm="3">
+                        <Card>
+                            <CardBody>
+                                <h5 className="text-muted mb-4"><i
+                                    className={"bx bx-rotate-right h1 text-primary align-middle mb-0 mr-3"}></i>Txn Count
+                                </h5>
+                                <Row>
+                                    <Col xs="6">
+                                        <div>
+                                            <h5>{+props.globalData?.addLiquidityTx + +props.globalData?.removeLiquidityTx + +props.globalData?.swapTx} TXN</h5>
+                                            <p className="text-muted text-truncate mb-0">0,0<i
+                                                className="mdi mdi-arrow-up ml-1 text-success"></i></p>
+                                        </div>
+                                    </Col>
+                                    <Col xs="6">
+                                        <div>
+                                            <div className="apex-charts">
+                                                <ReactApexChart options={options3} series={series3} type="area"
+                                                                height={40}/>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col sm="3">
+                        <Card>
+                            <CardBody>
+                                <h5 className="text-muted mb-4"><i
+                                    className={"bx bx-trending-up h1 text-success align-middle mb-0 mr-3"}></i>Total
+                                    Earnings</h5>
+                                <Row>
+                                    <Col xs="6">
+                                        <div>
+                                            <h5>{formatUSDStatBoxes(convertFromWei(props.globalData?.totalFees), context.spartanPrice)}</h5>
+                                            <p className="text-muted text-truncate mb-0">0,0<i
+                                                className="mdi mdi-arrow-up ml-1 text-success"></i></p>
+                                        </div>
+                                    </Col>
+                                    <Col xs="6">
+                                        <div>
+                                            <div className="apex-charts">
+                                                <ReactApexChart options={options4} series={series4} type="area"
+                                                                height={40}/>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                {/*<Row>*/}
+                {/*    <Col sm="3">*/}
+
+                {/*        <Card className="mini-stats-wid">*/}
+                {/*            <CardBody>*/}
+                {/*                <Media>*/}
+                {/*                    <Media body>*/}
+
+                {/*                        <p className="text-muted font-weight-medium">TOTAL POOLED</p>*/}
+                {/*                        <h4 className="mb-0">{formatUSDStatBoxes(convertFromWei(props.globalData.totalPooled * 2), context.spartanPrice)}</h4>*/}
+
+                {/*                    </Media>*/}
+                {/*                </Media>*/}
+                {/*            </CardBody>*/}
+                {/*        </Card>*/}
+                {/*    </Col>*/}
+                {/*    <Col sm="3">*/}
+                {/*        <Card className="mini-stats-wid">*/}
+                {/*            <CardBody>*/}
+                {/*                <Media>*/}
+                {/*                    <Media body>*/}
+                {/*                        <p className="text-muted font-weight-medium">TOTAL VOLUME</p>*/}
+                {/*                        <h4 className="mb-0">{formatUSDStatBoxes(convertFromWei(props.globalData?.totalVolume), context.spartanPrice)}</h4>*/}
+                {/*                    </Media>*/}
+                {/*                </Media>*/}
+                {/*            </CardBody>*/}
+                {/*        </Card>*/}
+                {/*    </Col>*/}
+                {/*    <Col sm="3">*/}
+                {/*        <Card className="mini-stats-wid">*/}
+                {/*            <CardBody>*/}
+                {/*                <Media>*/}
+                {/*                    <Media body>*/}
+                {/*                        <p className="text-muted font-weight-medium">TXN COUNT</p>*/}
+                {/*                        <h4 className="mb-0">{+props.globalData?.addLiquidityTx + +props.globalData?.removeLiquidityTx + +props.globalData?.swapTx}</h4>*/}
+                {/*                    </Media>*/}
+                {/*                </Media>*/}
+                {/*            </CardBody>*/}
+                {/*        </Card>*/}
+                {/*    </Col>*/}
+                {/*    <Col sm="3">*/}
+                {/*        <Card className="mini-stats-wid">*/}
+                {/*            <CardBody>*/}
+                {/*                <Media>*/}
+                {/*                    <Media body>*/}
+                {/*                        <p className="text-muted font-weight-medium">TOTAL EARNINGS</p>*/}
+                {/*                        <h4 className="mb-0">{formatUSDStatBoxes(convertFromWei(props.globalData?.totalFees), context.spartanPrice)}</h4>*/}
+                {/*                    </Media>*/}
+                {/*                </Media>*/}
+                {/*            </CardBody>*/}
+                {/*        </Card>*/}
+                {/*    </Col>*/}
+                {/*</Row>*/}
+            </React.Fragment>
+        )
+    }
+
+;
 
 
 const PoolTable = (props) => {
@@ -160,7 +364,7 @@ const PoolTable = (props) => {
     return (
         <>
             <Row>
-                <Col sm={12}>
+                <Col sm={12} className="mr-20">
                     <Card>
                         <CardBody>
                             {!context.poolsData &&
@@ -169,8 +373,8 @@ const PoolTable = (props) => {
 
                             {context.poolsData &&
 
-                            < div className="table-responsive">
-                                <CardTitle>Pool Table</CardTitle>
+                            <div className="table-responsive">
+                                <CardTitle><h3>Pool Table</h3></CardTitle>
                                 <Table className="table-centered mb-0">
 
                                     <thead className="center">
@@ -241,20 +445,20 @@ const PoolTableItem = (props) => {
                 <td className="d-none d-lg-table-cell">
                     {formatUSDStatBoxes(convertFromWei(props.fees), context.spartanPrice)}
                 </td>
-                    <td>
-                        <Link to={`/pool/stake?pool=${props.address}`}>
-                            <Button color="primary"
-                                    className="btn btn-primary waves-effect waves-light mr-4">
-                                <i className="bx bx-log-in-circle"></i> Join
-                            </Button>
-                        </Link>
-                        <Link to={`/pool/swap?pool=${props.address}`}>
-                            <button type="button"
-                                    className="btn btn-primary waves-effect waves-light">
-                                <i className="bx bx-transfer-alt"></i> Trade
-                            </button>
-                        </Link>
-                    </td>
+                <td>
+                    <Link to={`/pool/stake?pool=${props.address}`}>
+                        <Button color="primary"
+                                className="btn btn-primary waves-effect waves-light mr-4">
+                            <i className="bx bx-log-in-circle"></i> Join
+                        </Button>
+                    </Link>
+                    <Link to={`/pool/swap?pool=${props.address}`}>
+                        <button type="button"
+                                className="btn btn-primary waves-effect waves-light">
+                            <i className="bx bx-transfer-alt"></i> Trade
+                        </button>
+                    </Link>
+                </td>
 
             </tr>
         </>
